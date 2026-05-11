9e503433b2b44fb78a9f70d8a3c1711ae063df0b
Sofia Garcia <sofiaagarciaa223@gmail.com>
2026-05-11 17:49:34 -0300

    refactor(alumnos): se refactorizo con camel case

--- backend/src/routes/users/alumnos.rs
+++ backend/src/routes/users/alumnos.rs
@@ -23,6 +23,6 @@ pub fn router() -> Router {
 pub async fn leer_alumno(Path(user_id): Path<i32>) -> impl IntoResponse {
     format!("leyendo alumno {}", user_id) //aca iria la consulta sql
 }
-pub async fn crear_alumno(Json(jsonPersona): Json<Alumno>) -> impl IntoResponse {
-    format!("creando alumno {}", jsonPersona.mail) //aca iria la consulta sqlito
+pub async fn crear_alumno(Json(json_persona): Json<Alumno>) -> impl IntoResponse {
+    format!("creando alumno {}", json_persona.mail) //aca iria la consulta sqlito
 }
