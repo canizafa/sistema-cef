040f10433cd8629a79dfd9199ea8b96c537bdc83
Sofia Garcia <sofiaagarciaa223@gmail.com>
2026-05-11 17:40:59 -0300

    feat(app): se agregan rutas

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,1 +1,21 @@
-async fn main() {}
+//pense el main para levantar el servidor nomas, las otras responsabilidades las delego a otros archivos
+mod routes;
+
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let puerto = 8081; //elegido para no tener problemas
+    let dir = SocketAddr::from(([0, 0, 0, 0], puerto));
+    let listener = tokio::net::TcpListener::bind(dir)
+        .await
+        .unwrap_or_else(|e| {
+            panic!("fallo la conetsion de la dir {}:{}", dir, e);
+        });
+    let router = routes::root::router().merge(routes::users::alumnos::router());
+
+    axum::serve(listener, router).await.unwrap_or_else(|e| {
+        //esto levanta el servidor
+        panic!("fallo la conetsion con el servidor {}", e);
+    });
+}
---
+++ backend/src/routes/users/alumnos.rs
@@ -0,0 +1,28 @@
+use axum::{
+    Json, Router,
+    extract::Path,
+    response::IntoResponse,
+    routing::{get, post},
+};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Alumno {
+    //1 alumno
+    nombre: String,
+    apellido: String,
+    dni: String,
+    mail: String,
+    //planilla deberia ser otra entidad
+}
+pub fn router() -> Router {
+    Router::new()
+        .route("/api/users/{id}", axum::routing::get(leer_alumno))
+        .route("/alumnos", post(crear_alumno)) //GET
+}
+pub async fn leer_alumno(Path(user_id): Path<i32>) -> impl IntoResponse {
+    format!("leyendo alumno {}", user_id) //aca iria la consulta sql
+}
+pub async fn crear_alumno(Json(jsonPersona): Json<Alumno>) -> impl IntoResponse {
+    format!("creando alumno {}", jsonPersona.mail) //aca iria la consulta sqlito
+}
