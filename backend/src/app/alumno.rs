ecf044b6577d169a8ca3de74ce9c1b1b93ca444e
Facundo Alejandro Caniza <facundocaniza@proton.me>
2026-05-14 09:22:07 -0300

    refactor(alumno): se agrega el struct de creacion y se separa del struct de respuesta

--- backend/src/app/alumno.rs
+++ backend/src/app/alumno.rs
@@ -1,8 +1,18 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Alumno {
+pub struct CreateAlumno {
     //1 alumno
+    pub nombre: String,
+    pub apellido: String,
+    pub dni: String,
+    pub mail: String,
+    //planilla deberia ser otra entidad
+}
+
+#[derive(Deserialize)]
+pub struct Alumno {
+    pub id: u64,
     pub nombre: String,
     pub apellido: String,
     pub dni: String,
