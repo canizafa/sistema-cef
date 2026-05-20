e0fc4b816f9abb8480802bac155e274c7ab2f269
Facundo Caniza <facundocaniza@proton.me>
2026-05-20 14:50:10 -0300

    fix(app): se agrega la pool de conexiones en las rutas y la migraciones automaticas

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,18 +7,27 @@ mod routes;
 
 use std::net::SocketAddr;
 
+use sqlx::SqlitePool;
+
 #[tokio::main]
 async fn main() {
-    let puerto = 8081; //elegido para no tener problemas
+    let puerto = 8081;
     let dir = SocketAddr::from(([0, 0, 0, 0], puerto));
     let listener = tokio::net::TcpListener::bind(dir)
         .await
         .unwrap_or_else(|e| {
             panic!("fallo la conetsion de la dir {}:{}", dir, e);
         });
-    let router = routes::root::router();
 
-    axum::serve(listener, router).await.unwrap_or_else(|e| {
+    let pool = SqlitePool::connect(&std::env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
+
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+
+    let app = routes::root::router().with_state(pool);
+
+    axum::serve(listener, app).await.unwrap_or_else(|e| {
         //esto levanta el servidor
         panic!("fallo la conetsion con el servidor {}", e);
     });
--- backend/src/routes/root.rs
+++ backend/src/routes/root.rs
@@ -1,8 +1,9 @@
 use axum::{Router, routing::get};
+use sqlx::SqlitePool;
 
 use crate::routes::{alumnos::alumnos_router, auth::auth_router, health_checker::health_checker};
 
-pub fn router() -> Router {
+pub fn router() -> Router<SqlitePool> {
     Router::new()
         .route("/api", get(health_checker))
         .nest("/api/alumnos", alumnos_router()) // se anidan las rutas de alumnos que se trabajan en su propio archivo
