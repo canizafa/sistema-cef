2bc27d4c1e046e43316723e256fe04472afdcbda
Facundo Caniza <facundocaniza@proton.me>
2026-05-22 15:54:51 -0300

    fix()

--- backend/src/dtos/mod.rs
+++ backend/src/dtos/mod.rs
@@ -6,12 +6,3 @@ pub mod empleado_dto;
 pub mod ficha_medica_dto;
 pub mod lista_espera_dto;
 pub mod reserva_dto;
-
-pub use asistencia_dto::AsistenciaDto;
-pub use auth_dto::AuthDto;
-pub use clase_dto::ClaseDto;
-pub use cliente_dto::ClienteDto;
-pub use empleado_dto::EmpleadoDto;
-pub use ficha_medica_dto::FichaMedicaDto;
-pub use lista_espera_dto::ListaEsperaDto;
-pub use reserva_dto::ReservaDto;
