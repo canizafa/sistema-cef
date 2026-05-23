6ab14af61116c81825ebe0a03581ae7a437007b0
Facundo Caniza <facundocaniza@proton.me>
2026-05-23 10:22:00 -0300

    fix(routes): se arreglan direcciones de rutas

--- backend/src/routes/asistencia_route.rs
+++ backend/src/routes/asistencia_route.rs
@@ -15,5 +15,5 @@ pub fn asistencia_router() -> Router<SqlitePool> {
         .route("/get-asistencia", get(get_asistencia_handler()))
         .route("/update-asistencia", post(update_asistencia_handler()))
         .route("/delete-asistencia", post(delete_asistencia_handler()))
-        .route("get-asistencias", get(get_asistencias_handler()))
+        .route("/get-all", get(get_asistencias_handler()))
 }
