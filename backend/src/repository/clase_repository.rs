8ed4d0a2ece7d20e60e4cf13c4f24fca2d6c6c74
Sofia Garcia <sofiaagarciaa223@gmail.com>
2026-05-23 21:56:55 -0300

    se arreglan conflictos

--- backend/src/repository/clase_repository.rs
+++ backend/src/repository/clase_repository.rs
@@ -7,22 +7,42 @@ use crate::domain::Clase;
 pub struct ClaseRepository;
 
 impl ClaseRepository {
-    /*el flujo sería:
-    hacer el INSERT
-    recuperar la fila insertada
-    devolver Clase*/
+    /* el flujo sería:
+       hacer el INSERT
+       recuperar la fila insertada
+       devolver Clase
+    */
     pub async fn create_clase(
         State(pool): State<SqlitePool>,
         clase: Clase,
     ) -> Result<Option<Clase>, ApiError> {
+<<<<<<< HEAD
         sqlx::query_as!(Clase,
             "INSERT INTO clase (dia, horario, cupo_profe, cupo_maximo, estado, descripcion, id_sala, dni_profesor)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
                 RETURNING id_clase, dia, horario, cupo_profe, cupo_maximo, estado, descripcion, id_sala, dni_profesor",
+=======
+        sqlx::query_as!(
+            Clase,
+            "INSERT INTO clase
+            (dia, horario, cupo_profe, cupo_maximo, estado, descripcion, id_sala, dni_profesor)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            RETURNING
+                id_clase,
+                dia,
+                horario,
+                cupo_profe,
+                cupo_maximo,
+                estado,
+                descripcion,
+                id_sala,
+                dni_profesor",
+>>>>>>> 0070171 (se arreglaron las funciones)
             clase.get_dia(),
             clase.get_horario(),
             clase.get_cupo_profe(),
             clase.get_cupo_maximo(),
+<<<<<<< HEAD
             clase.get_estado(),
             clase.get_descripcion(),
             clase.get_id_sala(),
@@ -30,31 +50,72 @@ impl ClaseRepository {
             .fetch_one(&pool)
             .await
             .map_err(|e| ApiError::DatabaseError(e))
+=======
+            clase.get_estado() as _,
+            clase.get_descripcion(),
+            clase.get_id_sala(),
+            clase.get_dni_profesor()
+        )
+        .fetch_optional(&pool)
+        .await
+        .map_err(ApiError::DatabaseError)
+>>>>>>> 0070171 (se arreglaron las funciones)
     }
+
     pub async fn list_clases(
         State(pool): State<SqlitePool>,
     ) -> Result<Option<Vec<Clase>>, ApiError> {
         sqlx::query_as!(
             Clase,
+<<<<<<< HEAD
             "SELECT id_clase, dia,horario,
                 cupo_profe,cupo_maximo,estado,
                 descripcion,id_sala,dni_profesor
+=======
+            "SELECT
+                id_clase,
+                dia,
+                horario,
+                cupo_profe,
+                cupo_maximo,
+                estado,
+                descripcion,
+                id_sala,
+                dni_profesor
+>>>>>>> 0070171 (se arreglaron las funciones)
             FROM clase"
         )
         .fetch_all(&pool)
         .await
+<<<<<<< HEAD
         .map_err(|e| ApiError::DatabaseError(e))
+=======
+        .map_err(ApiError::DatabaseError)
+>>>>>>> 0070171 (se arreglaron las funciones)
         .map(Some)
     }
+
     pub async fn get_by_id(
         State(pool): State<SqlitePool>,
         id: i32,
     ) -> Result<Option<Clase>, ApiError> {
         sqlx::query_as!(
             Clase,
+<<<<<<< HEAD
             "SELECT id_clase,dia,horario,
                 cupo_profe,cupo_maximo,
                 estado,descripcion,id_sala,
+=======
+            "SELECT
+                id_clase,
+                dia,
+                horario,
+                cupo_profe,
+                cupo_maximo,
+                estado,
+                descripcion,
+                id_sala,
+>>>>>>> 0070171 (se arreglaron las funciones)
                 dni_profesor
             FROM clase
             WHERE id_clase = ?",
@@ -62,15 +123,23 @@ impl ClaseRepository {
         )
         .fetch_optional(&pool)
         .await
+<<<<<<< HEAD
         .map_err(|e| ApiError::DatabaseError(e))
     }
     pub async fn update_clase(
         //editar clase
+=======
+        .map_err(ApiError::DatabaseError)
+    }
+
+    pub async fn update_clase(
+>>>>>>> 0070171 (se arreglaron las funciones)
         State(pool): State<SqlitePool>,
         id: i32,
         clase: Clase,
     ) -> Result<Option<Clase>, ApiError> {
         sqlx::query_as!(
+<<<<<<< HEAD
             "UPDATE clase
                 SET dia = ?, horario = ?, cupo_profe = ?, cupo_maximo = ?,
                 estado = ?, descripcion = ?, id_sala = ?, dni_profesor = ?
@@ -91,6 +160,22 @@ impl ClaseRepository {
         let clase_actualizada = sqlx::query_as!(
             Clase,
             "SELECT id_clase,
+=======
+            Clase,
+            "UPDATE clase
+            SET
+                dia = ?,
+                horario = ?,
+                cupo_profe = ?,
+                cupo_maximo = ?,
+                estado = ?,
+                descripcion = ?,
+                id_sala = ?,
+                dni_profesor = ?
+            WHERE id_clase = ?
+            RETURNING
+                id_clase,
+>>>>>>> 0070171 (se arreglaron las funciones)
                 dia,
                 horario,
                 cupo_profe,
@@ -98,17 +183,34 @@ impl ClaseRepository {
                 estado,
                 descripcion,
                 id_sala,
+<<<<<<< HEAD
                 dni_profesor
             FROM clase
             WHERE id_clase = ?",
+=======
+                dni_profesor",
+            clase.get_dia(),
+            clase.get_horario(),
+            clase.get_cupo_profe(),
+            clase.get_cupo_maximo(),
+            clase.get_estado() as _,
+            clase.get_descripcion(),
+            clase.get_id_sala(),
+            clase.get_dni_profesor(),
+>>>>>>> 0070171 (se arreglaron las funciones)
             id
         )
         .fetch_optional(&pool)
         .await
+<<<<<<< HEAD
         .map_err(ApiError::DatabaseError)?;
 
         Ok(clase_actualizada)
+=======
+        .map_err(ApiError::DatabaseError)
+>>>>>>> 0070171 (se arreglaron las funciones)
     }
+
     pub async fn delete_clase(
         State(pool): State<SqlitePool>,
         id: i32,
@@ -116,6 +218,7 @@ impl ClaseRepository {
         sqlx::query_as!(
             Clase,
             "DELETE FROM clase
+<<<<<<< HEAD
                 WHERE id_clase = ?
                 RETURNING
                     id_clase,
@@ -127,10 +230,27 @@ impl ClaseRepository {
                     descripcion,
                     id_sala,
                     dni_profesor",
+=======
+            WHERE id_clase = ?
+            RETURNING
+                id_clase,
+                dia,
+                horario,
+                cupo_profe,
+                cupo_maximo,
+                estado,
+                descripcion,
+                id_sala,
+                dni_profesor",
+>>>>>>> 0070171 (se arreglaron las funciones)
             id
         )
         .fetch_optional(&pool)
         .await
+<<<<<<< HEAD
         .map_err(|e| ApiError::DatabaseError(e))
+=======
+        .map_err(ApiError::DatabaseError)
+>>>>>>> 0070171 (se arreglaron las funciones)
     }
 }
