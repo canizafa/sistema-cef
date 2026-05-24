3d62a4afb42bc1817ec1ed41a0d8cd7c339cc8c4
Facundo Caniza <facundocaniza@proton.me>
2026-05-24 12:36:54 -0300

    chore(app): se incorpora uuid

--- backend/src/domain/clase.rs
+++ backend/src/domain/clase.rs
@@ -1,9 +1,13 @@
 use chrono::NaiveDate;
+use uuid::Uuid;
 
-use crate::domain::Estado;
+use crate::{
+    domain::Estado,
+    dtos::{ClaseResponse, CreateClaseRequest},
+};
 
 pub struct Clase {
-    id_clase: i32,
+    id_clase: String,
     dia: NaiveDate,
     horario: String,
     cupo_profe: i32,
@@ -15,8 +19,8 @@ pub struct Clase {
 }
 
 impl Clase {
-    pub fn get_id(&self) -> i32 {
-        self.id_clase
+    pub fn get_id(&self) -> &str {
+        &self.id_clase
     }
     pub fn get_dia(&self) -> NaiveDate {
         self.dia
@@ -36,10 +40,37 @@ impl Clase {
     pub fn get_descripcion(&self) -> &str {
         &self.descripcion
     }
-    pub fn get_id_sala(&self) -> i32 {
-        self.id_sala
+    pub fn is_lleno(&self) -> bool {
+        self.cupo_profe >= self.cupo_maximo
     }
-    pub fn get_dni_profesor(&self) -> i32 {
-        self.dni_profesor
+}
+
+impl From<CreateClaseRequest> for Clase {
+    fn from(value: CreateClaseRequest) -> Self {
+        let id = Uuid::new_v4().to_string();
+        Self {
+            id_clase: id,
+            dia: value.dia,
+            horario: value.horario,
+            cupo_profe: value.cupo_profe,
+            cupo_maximo: value.cupo_maximo,
+            estado: value.estado,
+            descripcion: value.descripcion,
+            id_sala: value.id_sala,
+            dni_profesor: value.dni_profesor,
+        }
+    }
+}
+
+impl From<Clase> for ClaseResponse {
+    fn from(value: Clase) -> Self {
+        Self {
+            id_clase: value.id_clase.clone(),
+            dia: value.dia,
+            horario: value.horario.clone(),
+            estado: value.estado.clone(),
+            lleno: value.is_lleno(),
+            descripcion: value.descripcion.clone(),
+        }
     }
 }
--- backend/src/dtos/clase_dto.rs
+++ backend/src/dtos/clase_dto.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 
 use crate::domain::rol::Estado;
+use chrono::NaiveDate;
 
 #[derive(Debug, Deserialize)]
 pub struct CreateClaseRequest {
-    pub dia: String,
+    pub dia: NaiveDate,
     pub horario: String,
     pub cupo_profe: i32,
     pub cupo_maximo: i32,
@@ -12,14 +13,17 @@ pub struct CreateClaseRequest {
     pub id_actividad: i32,
     pub id_sala: i32,
     pub dni_profesor: i32,
+    pub descripcion: String,
 }
 
 #[derive(Debug, Serialize)]
 pub struct ClaseResponse {
-    pub id_clase: i32,
-    pub dia: String,
+    pub id_clase: String,
+    pub dia: NaiveDate,
     pub horario: String,
     pub estado: Estado,
+    pub lleno: bool,
+    pub descripcion: String,
 }
 
 #[derive(Debug, Serialize)]
