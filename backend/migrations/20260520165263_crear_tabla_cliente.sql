5937b1333d907dd6b6f27b96f4b2122fc8a61c2f
Sofia Garcia <sofiaagarciaa223@gmail.com>
2026-05-25 19:38:41 -0300

    implementacion de las funciones en cliente repository

--- backend/migrations/20260520165263_crear_tabla_cliente.sql
+++ backend/migrations/20260520165263_crear_tabla_cliente.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS cliente (
     dni_cliente INTEGER primary key,
     nombre_completo VARCHAR(100) not null,
     email VARCHAR(100) unique not null,
-    telefono VARCHAR(30),
+    telefono VARCHAR(30) not null,
     fecha_nacimiento TEXT not null,
     estado VARCHAR(30) not null,
     password VARCHAR(100) not null,
-    id_ficha TEXT,
+    id_ficha TEXT not null,
     FOREIGN KEY (id_ficha) REFERENCES ficha_medica(id_ficha)
 )
