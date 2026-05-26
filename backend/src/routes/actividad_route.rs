90fbcbaf6efb9d03f2ce4bbe4b019b4efa678471
Facundo Caniza <facundocaniza@proton.me>
2026-05-26 13:59:06 -0300

    fix(routes): se arreglan las rutas y los handlers

--- backend/src/routes/actividad_route.rs
+++ backend/src/routes/actividad_route.rs
@@ -1,13 +1,18 @@
-use axum::Router;
-use axum::routing::get;
-use axum::routing::post;
+use axum::routing::{delete, put};
+use axum::{Router, routing::get, routing::post};
 
 use crate::app_state::AppState;
 use crate::handlers::create_actividad_handler;
+use crate::handlers::delete_actividad_handler;
 use crate::handlers::get_actividad_handler;
+use crate::handlers::get_actividades_handler;
+use crate::handlers::update_actividad_handler;
 
 pub fn actividad_router() -> Router<AppState> {
     Router::new()
         .route("/create", post(create_actividad_handler))
         .route("/get-actividad/{id}", get(get_actividad_handler))
+        .route("/get-actividades", get(get_actividades_handler))
+        .route("/delete/{id}", delete(delete_actividad_handler))
+        .route("/update/{id}", put(update_actividad_handler))
 }
---
+++ backend/src/routes/sala_route.rs
@@ -0,0 +1,18 @@
+use axum::Router;
+
+use axum::routing::{delete, get, post, put};
+
+use crate::app_state::AppState;
+use crate::handlers::sala_handler::{
+    create_sala_handler, delete_sala_handler, get_sala_handler, get_salas_handler,
+    update_sala_handler,
+};
+
+pub fn sala_routes() -> Router<AppState> {
+    Router::new()
+        .route("/", post(create_sala_handler))
+        .route("/", get(get_salas_handler))
+        .route("/:id", get(get_sala_handler))
+        .route("/:id", put(update_sala_handler))
+        .route("/:id", delete(delete_sala_handler))
+}
