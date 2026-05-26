62f48b50bd19e595f65301700d0ef05f19eb8668
Facundo Caniza <facundocaniza@proton.me>
2026-05-26 20:06:11 -0300

    feat(routes): se añade la ruta para cambiar de contraseña

--- backend/src/routes/auth_route.rs
+++ backend/src/routes/auth_route.rs
@@ -1,7 +1,8 @@
 use axum::Router;
-use axum::routing::post;
+use axum::routing::{post, put};
 
 use crate::app_state::AppState;
+use crate::handlers::change_password_handler;
 use crate::handlers::login_handler;
 use crate::handlers::register_cliente_handler;
 use crate::handlers::register_empleado_handler;
@@ -10,6 +11,7 @@ use crate::handlers::reset_password_handler;
 pub fn auth_router() -> Router<AppState> {
     Router::new()
         .route("/login", post(login_handler))
+        .route("/change-password/{dni}", put(change_password_handler))
         .route("/reset-password", post(reset_password_handler))
         .route("/register-empleado", post(register_empleado_handler))
         .route("/register-cliente", post(register_cliente_handler))
