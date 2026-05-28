4157d3637dda9b515bc78730ddec8029f6262d7e
Florencia Lavie <florlavie13@gmail.com>
2026-05-28 12:10:27 -0300

    feat: activar / desactivar empleado , listar empleado
    feat: activar / desactivar empleado , listar empleado
    + Corrección de estilos

--- frontend/src/pages/admin/clientes/ClientesPage.tsx
+++ frontend/src/pages/admin/clientes/ClientesPage.tsx
@@ -1,4 +1,4 @@
 // Clientes — en construcción
 export function ClientesPage() {
-  return <div>Clientes — en construcción</div>;
+  return <h1 className="text-2xl font-bold text-primary mb-6 pt-8 pl-8">Gestión de Clientes - en construcción</h1>
 }
--- frontend/src/pages/admin/reportes/ReportesPage.tsx
+++ frontend/src/pages/admin/reportes/ReportesPage.tsx
@@ -1,4 +1,4 @@
 
 export function ReportesPage() {
-  return <div>Reportes — en construcción</div>;
+  return <h1 className="text-2xl font-bold text-primary mb-6 pt-8 pl-8 ">Gestión de Reportes - en construcción</h1>
 }
